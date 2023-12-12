b1c4494cd6955a39e382692e9b449cbec74ea792
Hrithik Barman <87934722+noobZhawk@users.noreply.github.com>
2023-12-12 14:55:16 +0600

    Add files via upload

---
+++ 30 problem solved.c
@@ -0,0 +1,15 @@
+
+
+
+
+
+<kaisa laga meri majak\>
+
+
+
+
+H     H      A       H     H      A
+H     H     A A      H     H     A A
+HHHHHHH    A   A     HHHHHHH    A   A
+H     H   A AAA A    H     H   A AAA A
+H     H  A       A   H     H  A       A
